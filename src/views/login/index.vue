62d97123df471a0aba3be2e222b98a0c22edd43d
llf000 <915428940@qq.com>
2024-09-24 10:33:40 +0800

    登录页布局小调整

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,39 +1,40 @@
 <template>
-  <div class="relative min-h-[100%] flex items-center justify-center">
-    <el-form ref="userRef" :model="userForm" status-icon :rules="rules" label-width="auto" size="large"
-      class="w-[400px] p-[30px] shadow-xl bg-white rounded-md">
-      <div class="text-center w-[100%] pb-[20px] font-bold">vue-admin-js</div>
-      <el-alert v-show="isLoginError" title="用户名或密码错误" type="error" show-icon @close="isLoginError = false" />
-      <el-form-item class="mt-[20px]" label="" prop="username">
-        <el-input v-model="userForm.username" type="text" autocomplete="off" placeholder="账户：admin">
-          <template #prefix>
-            <el-icon color="var(--el-color-primary)">
-              <User />
-            </el-icon>
-          </template>
-        </el-input>
-      </el-form-item>
-      <el-form-item label="" prop="password">
-        <el-input v-model="userForm.password" type="password" autocomplete="off" show-password placeholder="密码：admin">
-          <template #prefix>
-            <el-icon color="var(--el-color-primary)">
-              <Lock />
-            </el-icon>
-          </template>
-        </el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button class="w-full" type="primary" @click="handleForm">登&nbsp;录</el-button>
-      </el-form-item>
-    </el-form>
-    <div class="absolute bottom-[5px] h-[30px]">
+  <div class="h-[100%] flex flex-col items-center ">
+    <div class="flex grow justify-center items-center">
+      <el-form ref="userRef" :model="userForm" status-icon :rules="rules" label-width="auto" size="large"
+        class="w-[400px]  p-[30px] shadow-xl bg-white rounded-md">
+        <div class="text-center w-[100%] pb-[20px] font-bold">vue-admin-js</div>
+        <el-alert v-show="isLoginError" title="用户名或密码错误" type="error" show-icon @close="isLoginError = false" />
+        <el-form-item class="mt-[20px]" label="" prop="username">
+          <el-input v-model="userForm.username" type="text" autocomplete="off" placeholder="账户：admin">
+            <template #prefix>
+              <el-icon color="var(--el-color-primary)">
+                <User />
+              </el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="" prop="password">
+          <el-input v-model="userForm.password" type="password" autocomplete="off" show-password placeholder="密码：admin">
+            <template #prefix>
+              <el-icon color="var(--el-color-primary)">
+                <Lock />
+              </el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="w-full" type="primary" @click="handleForm">登&nbsp;录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class=" h-[30px]">
       Copyright © {{ new Date().getFullYear() }} vue-admin-js
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue'
 import { useActiveStore } from '@/stores/topmenu'
 const topStore = useActiveStore()
 const router = useRouter()
