5e167fed3cb0d177ec8c038080ddf8306e8cef97
llf000 <915428940@qq.com>
2024-09-29 23:12:53 +0800

    fix:修复面包屑点击工作台跳转bug

--- src/layout/breadcrumb.vue
+++ src/layout/breadcrumb.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="p-[20px]">
-    <el-breadcrumb separator="/" v-show="route.path !== '/workplace'">
+  <div class="p-[10px]" v-if="route.path !== '/workplace'">
+    <el-breadcrumb separator="/">
       <el-breadcrumb-item :to="{ path: '/workplace' }" @click="handleSider">工作台</el-breadcrumb-item>
       <el-breadcrumb-item :to="{ path: breadcrumb.path }" v-for="breadcrumb in breadcrumbList">{{ breadcrumb.label
         }}</el-breadcrumb-item>
@@ -10,9 +10,11 @@
 </template>
 
 <script setup>
+import { useActiveStore } from '@/stores/topmenu'
 import { useSideMenuStore } from '@/stores/sidemenu'
-const route = useRoute()
+const topStore = useActiveStore()
 const sideStore = useSideMenuStore()
+const route = useRoute()
 
 const breadcrumbList = computed(() => {
   const paths = route.matched.map(record => ({
@@ -25,7 +27,7 @@ const breadcrumbList = computed(() => {
 });
 
 const handleSider = () => {
-  // topStore.setKey(nextTab.path)
+  topStore.setKey(route.path)
   sideStore.setSideMenu([])
 }
 </script>
