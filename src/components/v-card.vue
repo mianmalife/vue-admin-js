7d1e44f3450171d156ba41f05d6c222e1df3bb5c
mianmalife <253590433@qq.com>
2025-06-11 17:05:23 +0800

    style: 卡片阴影，body背景修改优化

--- src/components/v-card.vue
+++ src/components/v-card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="v-card bg-white p-[10px] rounded-lg shadow">
+  <div class="v-card bg-white p-[10px] rounded-[var(--el-border-radius-base)] shadow-md">
     <slot></slot>
   </div>
 </template>
